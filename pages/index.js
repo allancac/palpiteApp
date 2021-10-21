346ac26e564edc3af278330850916f34843e5465
Allan Cezar Almeida Chaves <allancac@gmail.com>
2021-10-21 02:19:21 -0300

    index and pesquisa pages were fineshed

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
-
+import Link from 'next/link'
 
 const Index = () => {
   return (
-    <div>
+    <div className='tracking-wider uppercase text-center text-lg font-bold leading-10 my-8'>
+      <p >O nosso restaurante sempre busca o<br />
+        melhor atendimento aos nossos clientes.<br />
+        Por isso, estamos atentos à sua opinião/sugestão.
+      </p>
 
+      <div className='text-center my-8'>
+        <Link href='/pesquisa'>
+          <a className='px-6 py-4 bg-blue-300 rounded-md font-bold hover:bg-green-400'>DEIXAR OPINIÃO OU SUGESTÃO</a>
+        </Link>
+      </div>
 
+      <div className='text-center my-8 text-sm text-gray-400'>
+        <p>Ao dar sua opinião, ganhe <span>15</span>% na sua próxima compra</p>
+      </div>
     </div>
   )
 }
--- pages/pesquisa.js
+++ pages/pesquisa.js
@@ -4,7 +4,25 @@ import Link from 'next/link';
 const Pesquisa = () => {
   return (
     <div>
-      <h1>Pesquisa</h1>
+      <h1 className='tracking-wider uppercase text-center text-lg font-bold leading-10 my-8'>Críticas e Sugestões</h1>
+      <div className='flex justify-start my-6 md:w-600 mx-auto' >
+        <label className='px-4 text-lg w-1/3' htmlFor='userName'>Nome:</label><br/>
+        <input className='bg-blue-100 rounded-md h-6 w-full p-4 text-lg font-sans' type="text" id='userName'/>
+      </div>
+      <div className='flex justify-start my-6 md:w-600 mx-auto' >
+        <label className='px-4 text-lg w-1/3' htmlFor='userEmail'>E-mail:</label><br/>
+        <input className='bg-blue-100 rounded-md h-6 w-full p-4 text-lg font-sans' type="email" id='userEmail'/>
+      </div>
+      <div className='flex justify-start my-6 md:w-600 mx-auto' >
+        <label className='px-4 text-lg w-1/3' htmlFor='userWhatsapp'>WhatsApp:</label><br/>
+        <input className='bg-blue-100 rounded-md h-6 w-full p-4 text-lg font-sans' type="tel" id='userWhatsapp'
+        pattern='[0-9]{2} [0-9]{5}-[0-9]{4}' required/>
+      </div>
+      <div className='flex justify-start my-6 md:w-600 mx-auto' >
+        <label className='px-4 text-lg w-1/3' htmlFor='critica'>Crítica ou Sugestão:</label><br/>
+        <textarea className='bg-blue-100 rounded-md h-6 w-full p-4 text-lg font-sans' id='critica' rows='4' cols='50'/>
+
+      </div>
 
     </div>
   )
