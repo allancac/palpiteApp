6b8cdc4475b01ac98e9ff9a689be5f9c0f17ef07
Allan Cezar Almeida Chaves <allancac@gmail.com>
2021-10-21 02:14:38 -0300

    Footer Component added in the project

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -4,26 +4,26 @@ import styles from './styles.module.css'
 
 
 const Footer = () => {
-  return(
-    <div className='bg-gray-700'>
+  return (
+    <div className='bg-blue-900'>
       <div className='container mx-auto font-bold text-center text-white p-4'>
-          <span className='px-2'>
-            Projeto Desenvolvido por: Allan Chaves/
-          </span>
-          <span>
-            <Link href="https://www.linkedin.com/in/allancac/"><a className='hover:underline px-2'>Linkedin/</a></Link>
-          </span>
-          <span>
-            <Link href="https://github.com/allancac/palpiteApp"><a className='hover:underline px-2'>Github</a></Link>
-          </span>
+        <span className='px-2'>
+          Projeto Desenvolvido por: Allan Chaves/
+        </span>
+        <span>
+          <Link href="https://www.linkedin.com/in/allancac/"><a className='hover:underline px-2'>Linkedin/</a></Link>
+        </span>
+        <span>
+          <Link href="https://github.com/allancac/palpiteApp"><a className='hover:underline px-2'>Github</a></Link>
+        </span>
         <div className='mx-auto'>
-        <img className='inline p-4' src="./logo_semana_fsm.png" alt="Logotipo da Semana Full Satack" />
-        <img className='inline p-4' src="./logo_devpleno.png" alt="Logotipo DevPleno" />
+          <img className='inline p-4' src="./logo_semana_fsm.png" alt="Logotipo da Semana Full Satack" />
+          <img className='inline p-4' src="./logo_devpleno.png" alt="Logotipo DevPleno" />
         </div>
       </div>
 
     </div>
-    
+
   )
 
 }
